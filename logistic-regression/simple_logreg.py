d8c82c3ad02cc60af775e54298615735ceed0ce8
Maxrio <lasse@rosendahl-energietechnik.de>
2024-05-21 13:58:00 +0200

    changed names and added code with logistic regression, ROC curve and plotted coefficients

--- logistic-regression/simple_logreg.py
+++ logistic-regression/simple_logreg.py
@@ -1,7 +1,6 @@
 import seaborn
 import pandas as pd
 import numpy as np
-import codecademylib3
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
